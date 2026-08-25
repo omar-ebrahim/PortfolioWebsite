a5547805af5463b5d290dc39879e1e467215eecb
Omar Ebrahim <o_ebrahim1@outlook.com>
2026-08-25 11:15:50 +0100

    added svg

--- Portfolio.Web/src/pages/home/components/middlesection/MiddleSection.tsx
+++ Portfolio.Web/src/pages/home/components/middlesection/MiddleSection.tsx
@@ -1,16 +1,19 @@
 import styles from './MiddleSection.module.scss';
-
 import middleSectionJson from './middlesection.json';
+import Bicycle from '../../../../components/glyphs/Bicycle';
 
 const MiddleSection = () => {
     return (
         <div>
-            <div className={styles.heading}>About me</div>;
+            <div className={styles.heading}>About me</div>
             <div className={styles.content}>
                 <div className={styles.leftContent}>
                     {middleSectionJson.intro}
                 </div>
-                <div className={styles.rightContent}>world</div>
+                <div className={styles.rightContent}>
+                    {/* <img src={BikeSvg} /> */}
+                    <Bicycle className={styles.glyph} size={256} />
+                </div>
             </div>
         </div>
     );
