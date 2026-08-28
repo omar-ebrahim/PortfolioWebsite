d3b35ee6dc618cdbdb38bcbd6f9281718d839ec4
Omar Ebrahim <o_ebrahim1@outlook.com>
2026-08-28 14:36:35 +0100

    Revert "Added skeleton Sanity studio project (#6)"
    This reverts commit 59bb0ce8eaa9c702b8e4677c8d1062ae376e5a0d.

--- Portfolio.Web/src/App.tsx
+++ Portfolio.Web/src/App.tsx
@@ -1,20 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
-import StudioPage from './pages/studio/StudioPage';
 
 function App() {
     return (
-        <BrowserRouter
-            basename={
-                window.location.pathname.startsWith('/PortfolioWebsite')
-                    ? '/PortfolioWebsite'
-                    : undefined
-            }
-        >
+        <BrowserRouter basename="/PortfolioWebsite">
             <main>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/studio/*" element={<StudioPage />} />
                 </Routes>
             </main>
         </BrowserRouter>
