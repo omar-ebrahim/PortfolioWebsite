51119f93cc0e26cbb679f828b896c1ea2b0eb910
Omar Ebrahim <o_ebrahim1@outlook.com>
2026-09-07 09:18:12 +0100

    more CSS updates

--- Portfolio.Web/src/pages/home/Home.tsx
+++ Portfolio.Web/src/pages/home/Home.tsx
@@ -3,16 +3,19 @@ import AboutMe from './components/aboutme/AboutMe';
 import ContactMe from './components/contact-me/ContactMe';
 import HomeSection from './components/homesection/HomeSection';
 import Services from './components/services/Services';
+import styles from './Home.module.scss';
 
 const Home = () => {
 
     return (
         <>
             <Navbar />
-            <HomeSection />
-            <AboutMe />
-            <Services />
-            <ContactMe />
+            <main className={styles.layout}>
+                <HomeSection />
+                <AboutMe />
+                <Services />
+                <ContactMe />
+            </main>
         </>
     );
 };
